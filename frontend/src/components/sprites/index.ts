dc15261f94f34e9f2a7f37e693de7b21b74958b1
wink53 <77198827+wink53@users.noreply.github.com>
2025-12-24 14:23:09 +0000

    refactor

--- frontend/src/components/sprites/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all sprite components and types
-export * from './types';
-export { SpriteConfigForm } from './SpriteConfigForm';
-export { SpritePreview } from './SpritePreview';
-export { AnimationEditor } from './AnimationEditor';
